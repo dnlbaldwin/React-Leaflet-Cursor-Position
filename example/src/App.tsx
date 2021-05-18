48dc88c7225e8f72128d4946831022ab2546038d
Dan Baldwin <dnl.baldwin@gmail.com>
2021-05-17 21:07:01 -0400

    Rough implementation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,30 @@
-import { LayersControl, MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
+import { CSSProperties } from 'react';
+import { LayersControl, LayerGroup, MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 import CursorPosition from 'react-leaflet-cursor-position';
 // Ignore missing type definitions
 // @ts-ignore
 import { MgrsGraticule } from 'react-leaflet-mgrs-graticule';
+import { GarsGraticule } from 'react-leaflet-gars-graticule';
 
 import './App.css';
+
+const mgrsGraticuleName = 'MGRS';
+const garsGraticuleName = 'GARS';
+const overlayEnabled = true;
+
+const uiStyle: CSSProperties = {
+  marginBottom: '20px',
+  border: '1px solid rgba(0,0,0,0.2)',
+  borderRadius: '4px',
+  backgroundColor: '#FFFFFF',
+  outline: 'none',
+  fontSize: '14px',
+  boxShadow: 'none',
+  color: '#333',
+  padding: '2px 2px',
+  minHeight: '18px',
+  cursor: 'pointer',
+};
 
 function App() {
   return (
@@ -17,9 +37,7 @@ function App() {
         [-90, -180],
         [90, 180],
       ]}
-      zoomControl={false}
     >
-      <ZoomControl position="topright"></ZoomControl>
       <LayersControl position="topright">
         <LayersControl.BaseLayer checked name="ESRI Satellite">
           <TileLayer
@@ -33,15 +51,27 @@ function App() {
             attribution='&copy; <a href="https://wiki.openstreetmap.org/wiki/Esri"></a> contributors'
           />
         </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="OSM Topo">
+        <LayersControl.BaseLayer name="OSM">
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
         </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="OSM Topo">
+          <TileLayer url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png" attribution="OSM" />
+        </LayersControl.BaseLayer>
+        <LayersControl.Overlay checked={overlayEnabled} name={mgrsGraticuleName}>
+          <LayerGroup>
+            <MgrsGraticule name={mgrsGraticuleName} checked={overlayEnabled} />
+          </LayerGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name={garsGraticuleName}>
+          <LayerGroup>
+            <GarsGraticule name={garsGraticuleName} checked={!overlayEnabled} />
+          </LayerGroup>
+        </LayersControl.Overlay>
       </LayersControl>
-      <CursorPosition />
-      <MgrsGraticule />
+      <CursorPosition position={'leaflet-top lsft'} style={uiStyle} />
     </MapContainer>
   );
 }
--- src/CursorPosition.tsx
+++ src/CursorPosition.tsx
@@ -1,3 +1,4 @@
+import { GarsPrecision, llToGars } from 'gars-utils';
 import { LeafletMouseEvent, Map } from 'leaflet';
 // Ignore missing type definitions
 // @ts-ignore
@@ -9,99 +10,135 @@ enum GeoStandards {
   latLng,
   dms,
   mgrs,
+  gars,
 }
-// TODO - GARS
 let currentGeoStandard: GeoStandards = GeoStandards.mgrs;
 
+/**
+ * MGRS's forward function returns the MGRS string without any whitespace
+ * separating the component values.  This function splits them up.
+ *
+ * @param lat - Decimal Latitude value
+ * @param lng - Decimal Longitude value
+ * @returns MGRS String in format: (NNL LL NNNN NNNN)
+ */
+function llToFormattedMgrs(lat: number, lng: number) {
+  let temp = forward([lng, lat], 4);
+  const gzd = temp.slice(0, 3);
+  const hk = temp.slice(3, 5);
+  const easting = temp.slice(5, 9);
+  const northing = temp.slice(9, 13);
+  return gzd + ' ' + hk + ' ' + easting + ' ' + northing;
+}
+
+/**
+ * Takes a lat/lng and converts it to a specific standard, then formats
+ * and returns it as a string.
+ *
+ * @param lat - Decimal Latitude value
+ * @param lng - Decimal Longitude value
+ * @returns
+ */
 function getPosition(lat: number, lng: number) {
   let result: string = '';
   if (currentGeoStandard === GeoStandards.latLng) {
     const latCardinal = lat >= 0 ? 'N' : 'S';
     const lngCardinal = lng >= 0 ? 'E' : 'W';
-    result =
-      Math.abs(lng).toFixed(4).toString() + ' ' + lngCardinal + ' ' + lat.toFixed(4).toString() + ' ' + latCardinal;
+
+    let formattedLat = Math.abs(lat).toFixed(4).toString();
+
+    if (Math.abs(lat) < 10) {
+      formattedLat = '0' + formattedLat;
+    }
+
+    let formattedLng = Math.abs(lng).toFixed(4).toString();
+
+    if (Math.abs(lng) < 10) {
+      formattedLng = '00' + formattedLng;
+    } else if (Math.abs(lng) < 100) {
+      formattedLng = '0' + formattedLng;
+    }
+
+    result = formattedLat + ' ' + latCardinal + ' ' + formattedLng + ' ' + lngCardinal;
   } else if (currentGeoStandard === GeoStandards.dms) {
     result = convertDMS(lat, lng);
   } else if (currentGeoStandard === GeoStandards.mgrs) {
-    let temp = forward([lng, lat], 4);
-    const gzd = temp.slice(0, 3);
-    const hk = temp.slice(3, 5);
-    const easting = temp.slice(5, 9);
-    const northing = temp.slice(9, 13);
-
-    return gzd + ' ' + hk + ' ' + easting + ' ' + northing;
+    result = llToFormattedMgrs(lat, lng);
+  } else if (currentGeoStandard === GeoStandards.gars) {
+    result = llToGars({ lat: lat, lng: lng }, GarsPrecision.FiveMinutes);
   } else {
     console.error('Unknown GeoStandard selected');
   }
   return result;
 }
 
-function toDegreesMinutesAndSeconds(coordinate: number) {
+function toDegreesMinutesAndSeconds(coordinate: number, isLat: boolean) {
   let absolute = Math.abs(coordinate);
-  let degrees = Math.floor(absolute);
-  let minutesNotTruncated = (absolute - degrees) * 60;
-  let minutes = Math.floor(minutesNotTruncated);
-  let seconds = ((minutesNotTruncated - minutes) * 60).toFixed(0);
+  let degrees = Math.floor(absolute).toString();
+  let minutesNotTruncated = (absolute - Math.floor(absolute)) * 60;
+  let minutes = Math.floor(minutesNotTruncated).toString();
+  let seconds = ((minutesNotTruncated - Math.floor(minutesNotTruncated)) * 60).toFixed(0).toString();
+
+  if (isLat) {
+    if (Math.abs(coordinate) < 10) {
+      degrees = '0' + degrees;
+    }
+  } else {
+    if (Math.abs(coordinate) < 10) {
+      degrees = '00' + degrees;
+    } else if (Math.abs(coordinate) < 100) {
+      degrees = '0' + degrees;
+    }
+  }
+
+  if (minutes.length < 2) {
+    minutes = '0' + minutes;
+  }
+
+  if (seconds.length < 2) {
+    seconds = '0' + seconds;
+  }
+
   return degrees + '° ' + minutes + "' " + seconds + '"';
 }
 
 function convertDMS(lat: number, lng: number) {
-  const latitude = toDegreesMinutesAndSeconds(lat);
+  const latitude = toDegreesMinutesAndSeconds(lat, true);
   const latitudeCardinal = lat >= 0 ? 'N' : 'S';
 
-  const longitude = toDegreesMinutesAndSeconds(lng);
+  const longitude = toDegreesMinutesAndSeconds(lng, false);
   const longitudeCardinal = lng >= 0 ? 'E' : 'W';
 
   return latitude + ' ' + latitudeCardinal + ' ' + longitude + ' ' + longitudeCardinal;
 }
 
-const CursorPosition = () => {
-  let map: Map = useMap();
+// Allowed positions the UI can occupy from the leaflet API
+const allowedPositions = [
+  'leaflet-bottom leaflet-right',
+  'leaflet-bottom leaflet-left',
+  'leaflet-top leaflet-right',
+  'leaflet-top leaflet-left',
+];
 
-  const [currentCursorLatLng, setCurrentCursorLatLng] = useState({ lat: 0, lng: 0 });
+interface propsType {
+  position: string;
+  style: CSSProperties;
+}
 
-  const [currentCentreLatLng, setCurrentCentreLatLng] = useState({ lat: 0, lng: 0 });
+/**
+ * CursorPosition - Functional component which displays the cursor and map position
+ * @returns
+ */
+const CursorPosition = (props: propsType) => {
+  const map: Map = useMap();
 
-  const [cursorPosition, setCursorPosition] = useState('');
+  const uiPosition = allowedPositions.includes(props.position) ? props.position : 'leaflet-bottom leaflet-right';
 
-  const [centrePosition, setCentrePosition] = useState(forward([map.getCenter().lng, map.getCenter().lat], 4));
-
-  function changeGeoStandard() {
-    if (currentGeoStandard === GeoStandards.mgrs) {
-      currentGeoStandard = GeoStandards.latLng;
-    } else if (currentGeoStandard === GeoStandards.latLng) {
-      currentGeoStandard = GeoStandards.dms;
-    } else if (currentGeoStandard === GeoStandards.dms) {
-      currentGeoStandard = GeoStandards.mgrs;
-    }
-    setCentrePosition(getPosition(currentCursorLatLng.lat, currentCursorLatLng.lng));
-    setCursorPosition(getPosition(currentCentreLatLng.lat, currentCentreLatLng.lng));
-  }
-
-  useMapEvents({
-    mousemove: (e: LeafletMouseEvent) => {
-      let latLng = e.latlng;
-      setCurrentCursorLatLng(latLng);
-      setCursorPosition(getPosition(latLng.lat, latLng.lng));
-
-      latLng = map.getCenter();
-      setCurrentCentreLatLng(latLng);
-      setCentrePosition(getPosition(latLng.lat, latLng.lng));
-    },
-    move: () => {
-      const latLng = map.getCenter();
-      setCentrePosition(getPosition(latLng.lat, latLng.lng));
-    },
-    mouseout: () => {
-      setCursorPosition('Off Map');
-    },
-  });
-
-  let style: CSSProperties = {
+  const defaultStyle: CSSProperties = {
     marginBottom: '20px',
     border: '1px solid rgba(0,0,0,0.2)',
     borderRadius: '4px',
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    backgroundColor: '#FFFFFF',
     outline: 'none',
     fontSize: '14px',
     boxShadow: 'none',
@@ -111,8 +148,53 @@ const CursorPosition = () => {
     cursor: 'pointer',
   };
 
+  const style = props.style || defaultStyle;
+
+  // Storing the proper lat/lng in state becase we need to be able to convert the position
+  // when the geo standard is changed
+  const [currentCursorLatLng, setCurrentCursorLatLng] = useState({ lat: 0, lng: 0 });
+  const [currentCentreLatLng, setCurrentCentreLatLng] = useState({ lat: 0, lng: 0 });
+
+  // The exact values being displayed in the component.  It's needed to have a second set of variables
+  // to track this as it won't account for events where the cursor is off map.
+  const [cursorPosition, setCursorPosition] = useState('');
+  const [centrePosition, setCentrePosition] = useState(llToFormattedMgrs(map.getCenter().lat, map.getCenter().lng));
+
+  function changeGeoStandard() {
+    if (currentGeoStandard === GeoStandards.mgrs) {
+      currentGeoStandard = GeoStandards.gars;
+    } else if (currentGeoStandard === GeoStandards.gars) {
+      currentGeoStandard = GeoStandards.latLng;
+    } else if (currentGeoStandard === GeoStandards.latLng) {
+      currentGeoStandard = GeoStandards.dms;
+    } else if (currentGeoStandard === GeoStandards.dms) {
+      currentGeoStandard = GeoStandards.mgrs;
+    }
+    setCentrePosition(getPosition(currentCentreLatLng.lat, currentCentreLatLng.lng));
+    setCursorPosition(getPosition(currentCursorLatLng.lat, currentCursorLatLng.lng));
+  }
+
+  // Specific map events which will result in an update to the component
+  useMapEvents({
+    mousemove: (e: LeafletMouseEvent) => {
+      let latLng = e.latlng;
+      setCurrentCursorLatLng(latLng);
+      setCursorPosition(getPosition(latLng.lat, latLng.lng));
+
+      setCurrentCentreLatLng(map.getCenter());
+      setCentrePosition(getPosition(map.getCenter().lat, map.getCenter().lng));
+    },
+    move: (e: any) => {
+      const latLng = map.getCenter();
+      setCentrePosition(getPosition(latLng.lat, latLng.lng));
+    },
+    mouseout: () => {
+      setCursorPosition('Off Map');
+    },
+  });
+
   return (
-    <div className="leaflet-bottom leaflet-right">
+    <div className={uiPosition}>
       <div className="leaflet-control leaflet-bar" style={style}>
         <table>
           <tbody>
